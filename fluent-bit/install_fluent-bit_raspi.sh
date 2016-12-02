dc7b6904bfb54af491ddce5e828f7f11dfe4279d
takahasi <takahasi@example.com>
2016-12-02 08:33:54 +0000

    [fix] Fixed invalid repository URL

--- fluent-bit/install_fluent-bit_raspi.sh
+++ fluent-bit/install_fluent-bit_raspi.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-wget http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_1_1_2/OpenRTM-aist/installer/install_scripts/pkg_install_debian.sh
+sudo apt-get update
 
-sudo sh pkg_install_python_debian.sh
+if [ ! type "wget" > /dev/null 2>&1 ]; then
+  sudo apt-get install wget
+fi
+
+if [ ! type "git" > /dev/null 2>&1 ]; then
+  sudo apt-get install git
+fi
+
+if [ ! type "cmake" > /dev/null 2>&1 ]; then
+  sudo apt-get install cmake
+fi
+
+
+wget http://svn.openrtm.org/OpenRTM-aist/tags/RELEASE_1_1_2/OpenRTM-aist/build/pkg_install_debian.sh
+sudo sh pkg_install_debian.sh
 
 git clone https://github.com/fluent/fluent-bit.git
 
