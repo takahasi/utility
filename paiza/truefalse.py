913c403335d6225854c42b64b15658ca2c806e80
takahasi <takahasi@sample.com>
2017-04-29 00:37:58 +0900

    [add] Add sample program

---
+++ paiza/truefalse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" script for generate process template directories
+
+This is xxx
+"""
+
+if __name__ == '__main__':
+    print str(raw_input()).replace('False', 'True')
