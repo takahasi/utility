d4027e08e3c1f2c8e209f86df87e0241f8165a35
takahasi <takahasi@sample.com>
2017-04-29 00:43:12 +0900

    [add] Add sample program

---
+++ paiza/choco.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" script for generate process template directories
+
+This is xxx
+"""
+
+if __name__ == '__main__':
+    l = raw_input().upper().split()
+    c = int(l[0])
+
+    if c == 0:
+        print '1'
+    else:
+        print c * 3
