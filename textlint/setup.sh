786012a8d900c21cefdc238d8c1be2937069cf31
takahasi <3263ta@gmail.com>
2017-11-06 14:35:28 +0900

    textlint: add common-spellcheck plugin

--- textlint/setup.sh
+++ textlint/setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+set -ue
+
+packages="textlint \
+    textlint-rule-preset-japanese \
+    textlint-rule-prh \
+    textlint-rule-preset-ja-technical-writing \
+    textlint-rule-common-misspellings \
+    textlint-rule-spellcheck-tech-word"
+
+
 if ! type npm > /dev/null 2>&1; then
   sudo apt-get install -y npm
 fi
 
-sudo npm i -g textlint textlint-rule-preset-japanese textlint-rule-prh textlint-rule-preset-ja-technical-writing textlint-rule-spellcheck-tech-word
+sudo npm i -g $packages
 
 if ! type node > /dev/null 2>&1; then
   sudo ln -s /usr/bin/nodejs /usr/bin/node
@@ -12,24 +22,21 @@ fi
 
 rm -rf prh.yml
 git clone https://github.com/azu/prh.yml.git
-cp -rf prh.yml/ja/jser-info.yml .
-cp -rf prh.yml/ja/typo.yml .
-cp -rf prh.yml/ja/web+db.yml .
-cp -rf prh.yml/ja/kanji-open.yml .
 
 cat << EOS > .textlintrc
 {
     "rules": {
+        "common-misspellings": true,
         "preset-japanese": true,
         "spellcheck-tech-word": true,
         "preset-ja-technical-writing": true,
         "prh": {
             "rulePaths": [
-                "./jser-info.yml",
-                "./spoken.yml",
-                "./typo.yml",
-                "./web+db.yml",
-                "./kanji-open.yml"
+                "./prh.yml/ja/jser-info.yml",
+                "./prh.yml/ja/spoken.yml",
+                "./prh.yml/ja/typo.yml",
+                "./prh.yml/ja/web+db.yml",
+                "./prh.yml/ja/kanji-open.yml"
             ]
         }
     }
